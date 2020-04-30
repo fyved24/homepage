c0812a949c6eb446ec3149fd32cb7d7eaa8fc5fe
fyved24 <fyved24@163.com>
2020-04-30 13:15:38 +0800

    添加备案信息

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import styles from './style.module.css'
 
 export default function Footer() {
     return (
-        <footer>
-            <a href="https://github.com/fyved24">github</a>
+        <footer  >
+            <div className={styles.beian}>
+
+                <a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=41010202002837" rel="noopener noreferrer" target="_blank">豫公网安备 41010202002837号</a>
+                <p>
+                Copyright © 2018-2020 ironz.cn | <a href="http://www.beian.miit.gov.cn/" rel="noopener noreferrer" target="_blank">豫ICP备20004176号-1</a>
+                </p>
+            </div>
+
         </footer>
     )
 }
