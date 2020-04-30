83bdd3b3d3575ba6cde0628a6c941ff73e205579
fyved24 <fyved24@163.com>
2020-04-30 09:23:28 +0800

    完成布局

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Project from './Project';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layout">
+      <main>
+        <div className="project_contianer">
+          <Project>项目1</Project>
+          <Project>项目2</Project>
+          <Project>项目3</Project>
+        </div>
+      </main>
+      <Footer></Footer>
     </div>
   );
 }
